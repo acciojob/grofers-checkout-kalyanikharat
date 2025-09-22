33f8b8e3301f2aa81e2f5b795d2d69d1b361b344
kalyanikharat <132137407+kalyanikharat@users.noreply.github.com>
2025-09-22 10:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices = document.querySelectorAll("price");
+
+	let totasum = 0;
+	prices.forEach((priceEL)=>{
+		totalsum = parseFloat(priceEL.textcontent);
+	});
+
+	let newrow = document.createElement("tr");
+	let totalcell = document.creatElement("td");
+
+	totalCell.setAttribute("colspan", "2");
+    totalCell.style.textAlign = "center";
+    totalCell.textContent = `Total Price: Rs ${total}`;
+
+	newrow.append(totalcell);
+
+	let table = document.querySelector('table');
+	table.appenChild('newrow');
+	
+	
   
 };
 
